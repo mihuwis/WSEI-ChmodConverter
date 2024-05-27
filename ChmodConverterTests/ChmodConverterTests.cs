240d5b94d42190282b83e6b93ab21cb319d6cab1
mihuwis <mihuwis@gmail.com>
2024-05-27 21:22:07 +0200

    Initial tests

---
+++ ChmodConverterTests/ChmodConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChmodConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChmodConverter.Tests
+{
+    [TestClass()]
+    public class ChmodConverterTests
+    {
+        [TestMethod()]
+        public void SymbolicToNumericTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NumericToSymbolicTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
