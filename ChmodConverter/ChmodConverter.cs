e95e3c4e9e082b3866c4529eb77dd43b578888f4
mihuwis <mihuwis@gmail.com>
2024-05-27 21:18:59 +0200

    Initial commit

---
+++ ChmodConverter/ChmodConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChmodConverter
+{
+    public static class ChmodConverter
+    {
+
+        public static int SymbolicToNumeric(string symbolicValue)
+        {
+            return 0;
+        }
+
+        public static string NumericToSymbolic(int numericValue)
+        {
+            return "";
+        }
+    }
+}
