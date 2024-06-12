d3bebab2696d9c0fa319b7f467f0ce26a289d961
mihuwis <mihuwis@gmail.com>
2024-06-12 19:18:03 +0200

    Change layot of input box 01

--- WpfInterface/MainWindow.xaml.cs
+++ WpfInterface/MainWindow.xaml.cs
@@ -11,9 +11,7 @@ using System.Windows.Shapes;
 
 namespace WpfInterface
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
